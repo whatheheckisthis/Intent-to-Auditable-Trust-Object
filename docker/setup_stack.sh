6b75b10c184fdbcad57d832201a7fcbf959eaa45
Dhruv | Security Operations | Assurance Professional <91457220+whatheheckisthis@users.noreply.github.com>
2026-02-16 11:20:21 +1300

    Add lean Nginx + PHP-FPM container stack (#19)

--- docker/setup_stack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running certificate integrity check..."
-./check_integrity.sh
-
-echo "Starting Docker Compose stack..."
-docker compose -f compose/docker-compose.yml up -d
